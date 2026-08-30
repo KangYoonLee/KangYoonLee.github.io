c89235be5c49c6bfae4f763b8003db5229e3e6a1
Kay <kangyoon.kay.lee@gmail.com>
2026-08-30 17:19:07 -0400

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -90,7 +90,7 @@ const publications = [
   },
   {
     year: "2023",
-    venue: "NCAEIC · Oral · Best Paper Award",
+    venue: "ICAEIC · Oral · Best Paper Award",
     title: "A Comparative Study of Synthetic Data with Generative AI Models: Safely Reproducing Sensitive Transportation Worker Data",
     authors: "Kangyoon Lee, Hanbin Ryou, Jihwan Baek, Sejong Oh, and Illchul Doo",
   },
